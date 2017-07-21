91955e47afb477ea3126b3476797ce2049b6e3d1
José Loarca <joseloarca97@icloud.com>
2017-07-20 22:58:00 -0600

    probando validaciones de usuario logueado

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -5,12 +5,16 @@ from .models import Registrado
 
 # Create your views here.
 def inicio(request):
+    titulo = "Formulario Registro"
+    if request.user.is_authenticated():
+        titulo = "Bienvenido %s" %(request.user)
     form = RegForm(request.POST or None)
     if form.is_valid():
         nombre = form.cleaned_data['nombre']
         email = form.cleaned_data['email']
         obj = Registrado.objects.create(email=email, nombre=nombre)
     context = {
+        "titulo":titulo,
         "form": form,
     }
     return render(request, "inicio.html", context)
