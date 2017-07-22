b048aadd33205d4bbe3b8e745607da0c27d156ec
José Loarca <joseloarca97@icloud.com>
2017-07-22 17:06:17 -0600

    Guardando datos provenientes de form

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .forms import RegForm
+from .forms import RegForm, RegModelForm
 from .models import Registrado
 
 # Create your views here.
@@ -8,13 +8,24 @@ def inicio(request):
     titulo = "Formulario Registro"
     if request.user.is_authenticated():
         titulo = "Bienvenido %s" %(request.user)
-    form = RegForm(request.POST or None)
-    if form.is_valid():
-        nombre = form.cleaned_data['nombre']
-        email = form.cleaned_data['email']
-        obj = Registrado.objects.create(email=email, nombre=nombre)
+    form = RegModelForm(request.POST or None)
+
     context = {
-        "titulo":titulo,
+        "titulo": titulo,
         "form": form,
     }
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        if not instance.nombre:
+            user, proveedor = instance.email.split("@")
+            instance.nombre = user
+        instance.save()
+        print(instance)
+        print(instance.timestamp)
+
+    context = {
+        "titulo": "Gracias por registrarte!",
+    }
+
     return render(request, "inicio.html", context)
