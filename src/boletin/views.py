8b6ed1e3833f8a8b072651de9ddb0098ccd357fb
José Loarca <joseloarca97@icloud.com>
2017-07-24 00:10:39 -0600

    Configurando HTML base & Bootstrap

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -31,7 +31,7 @@ def inicio(request):
         "titulo": "Gracias por registrarte!",
     }
 
-    return render(request, "inicio.html", context)
+    return render(request, "base.html", context)
 
 
 def contact(request):
